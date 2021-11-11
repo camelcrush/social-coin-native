bc721a59a66db9de4d55fd63396de83cac6c66d4
camelcrush <dkxm0927@naver.com>
2021-11-12 00:16:45 +0900

    onSubmitEditing: useRef for next input focus

--- screens/Join.js
+++ screens/Join.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components/native";
 
 const Container = styled.View``;
@@ -6,11 +6,16 @@ const Text = styled.Text``;
 const TextInput = styled.TextInput``;
 
 const Join = () => {
+  const passwordInput = useRef();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const onSubmitEditing = () => {
+    passwordInput.current.focus();
+  };
   return (
     <Container>
       <TextInput
+        onSubmitEditing={onSubmitEditing}
         onChangeText={(text) => setEmail(text)}
         keyboardType="email-address"
         autoCapitalize="none"
@@ -21,6 +26,7 @@ const Join = () => {
         placeholderTextColor={"rgba(255, 255, 255, 0.7)"}
       />
       <TextInput
+        ref={passwordInput}
         onChangeText={(text) => setPassword(text)}
         returnKeyType="done"
         placeholder="Password"
